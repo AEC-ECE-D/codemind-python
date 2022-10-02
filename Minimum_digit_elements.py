f59d947a335db506348675eb70c6cf7e364c1749
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-10-02 20:41:45 +0530

    n=int(input()) m=list(map(int,input().strip().split()))[:n] g=[] h=100 count=0 for i in range(len(m)):     c=abs(m[i])     a=str(c)     b=len(a)     if b<h:         h=b for j in range(len(m)):     c=abs(m[j])     a=str(c)     b=len(a)     if b==h:         count+=1 print(count)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,18 @@
+n=int(input())
+m=list(map(int,input().strip().split()))[:n]
+g=[]
+h=100
+count=0
+for i in range(len(m)):
+    c=abs(m[i])
+    a=str(c)
+    b=len(a)
+    if b<h:
+        h=b
+for j in range(len(m)):
+    c=abs(m[j])
+    a=str(c)
+    b=len(a)
+    if b==h:
+        count+=1
+print(count)
