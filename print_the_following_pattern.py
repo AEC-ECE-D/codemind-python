e626ddb93745fdbb9bce19f59e48e0b3177ac989
vivek kethala <84434941+AEC-ECE-D@users.noreply.github.com>
2022-10-09 13:09:29 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,n+1):         if j==i or j==(n+1-i):             print('x',end='')         else:             print(0,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,8 @@
 n=int(input())
 for i in range(1,n+1):
     for j in range(1,n+1):
-        if i==j or j==1 or j==n:
-            print('*',end=' ')
+        if j==i or j==(n+1-i):
+            print('x',end='')
         else:
-            print(' ',end=' ')
+            print(0,end='')
     print()
